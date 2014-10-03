1824786efd4412b9380f5caec1b3b6db9581a009
Marco Chareyron <chare@yron.me>
2014-10-03 14:48:26 +0200

    Initial commit

---
+++ BGColorAppearanceTest/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  BGColorAppearanceTest
+//
+//  Created by Marco Chareyron on 03/10/14.
+//  Copyright (c) 2014 Yron Lab. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
